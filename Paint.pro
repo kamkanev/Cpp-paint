9cc136935137b027cbcc9e81ba7dd1d30f839d5b
Kamen <kamkanev@gmail.com>
2024-06-03 16:52:25 +0200

    added QT GUI libs and user can draw now

--- Paint.pro
+++ Paint.pro
@@ -1,14 +1,18 @@
+QT += core gui
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
 TEMPLATE = app
-CONFIG += console c++14
-CONFIG -= app_bundle
-CONFIG -= qt
+CONFIG += console c++17
+# CONFIG -= app_bundle
+# CONFIG -= qt
 CONFIG += link_pkgconfig
 PKGCONFIG += opencv4
 
-# INCLUDEPATH += /usr/include/opencv4
+INCLUDEPATH += /usr/include/opencv4
 #INCLUDEPATH += /home/kamen/miniconda3/include/opencv4/opencv2
 
-# LIBS += -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs
+#LIBS += -lopencv_core -lopencv_highgui -lopencv_imgproc -lopencv_imgcodecs
 
 # wxCXXFLAGS = $$system(wx-config --cxxflags --unicode=yes --debug=no)
 # wxLinkOptions = $$system(wx-config --debug=no --libs --unicode=yes)
@@ -18,7 +22,12 @@ PKGCONFIG += opencv4
 
 SOURCES += \
         imagefilter.cpp \
-        main.cpp
+        main.cpp \
+        mainwindow.cpp
 
 HEADERS += \
-    imagefilter.h
+    imagefilter.h \
+    mainwindow.h
+
+FORMS += \
+    mainwindow.ui
--- main.cpp
+++ main.cpp
@@ -1,14 +1,36 @@
-#include "imagefilter.h"
+#include <imagefilter.h>
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <mainwindow.h>
+
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QInputDialog>
+#include <QtWidgets/QMessageBox>
+
 
 using namespace cv;
 using namespace std;
 
-int main()
+bool is_pressed = false;
+
+
+Mat canvas(1000, 1000, CV_8UC3,
+           Scalar(255, 255, 255));
+
+Mat drawimage(500, 500, CV_8UC3,
+          Scalar(255, 255, 255));
+
+Mat image(500, 500, CV_8UC3,
+          Scalar(255, 255, 255));
+
+void CallbackMouse (int event, int x, int y, int flags, void* userdata);
+
+void OnSqrButtonClick(int state, void* userdata);
+
+int main(int argc, char **argv)
 {
-    Mat image(500, 500, CV_8UC3,
-              Scalar(255, 255, 255));
+
+    drawimage.copyTo(image);
 
     // Check if the image is created
     // successfully or not
@@ -43,27 +65,88 @@ int main()
 
     //imwrite("/home/kamen/git/Cpp-paint/lenna2.png", image);
     // Show our image inside a window
-    namedWindow("Output", WINDOW_NORMAL);
 
-    int tr1 = createTrackbar("PointTop","Output", &pb.x, 500);
+    //WINDOW setup
+    String windowTitle = "Paint";
+    namedWindow(windowTitle, WINDOW_NORMAL);
+    setMouseCallback(windowTitle, CallbackMouse, NULL);
+
+    //create tracker
+    int tr1 = createTrackbar("PointTop", windowTitle, &pb.x, 500);
     cout<<tr1<<endl;
 
+    //create buttons
+    //createButton("Make Square", OnSqrButtonClick, NULL, 0, 0);
+    //cout<< getBuildInformation() << endl;
+
     while(true){
+
+        //input
+
+        //draw
 
         rectangle(image, pt, pb,
                   Scalar(255, 0, 0),
                   thickness, LINE_8);
 
         circle(image, pb, 5, Scalar(255, 0, 0), thickness, LINE_8, 1);
+        //show
 
-        imshow("Output", image);
+        imshow(windowTitle , image);
 
+
+        //update
         waitKey(1);
 
-
-        image = Mat(500, 500, CV_8UC3,
-                    Scalar(255, 255, 255));
+        //clean
+         // image = Mat(500, 500, CV_8UC3,
+         //             Scalar(255, 255, 255));
+        drawimage.copyTo(image);
     }
 
     return 0;
+}
+
+void CallbackMouse (int event, int x, int y, int flags, void* userdata){
+
+    if  ( event == EVENT_LBUTTONDOWN )
+    {
+        is_pressed = true;
+        //cout << "Left button of the mouse is pressed - position (" << x << ", " << y << ")" << endl;
+
+    }
+    else if  ( event == EVENT_RBUTTONDOWN )
+    {
+        //cout << "Right button of the mouse is pressed - position (" << x << ", " << y << ")" << endl;
+    }
+    else if  ( event == EVENT_MBUTTONDOWN )
+    {
+       // cout << "Middle button of the mouse is pressed - position (" << x << ", " << y << ")" << endl;
+    }
+    else if ( event == EVENT_MOUSEMOVE )
+    {
+       // cout << "Mouse move over the window - position (" << x << ", " << y << ")" << endl;
+        if (is_pressed){
+            circle(drawimage, Point(x, y), 5, Scalar(255, 0, 0), FILLED, LINE_8, 0);
+        }
+
+    }
+    else if  ( event == EVENT_LBUTTONUP )
+    {
+        is_pressed = false;
+      //  cout << "Left button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+    }
+    else if  ( event == EVENT_RBUTTONUP )
+    {
+      //  cout << "Right button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+    }
+    else if  ( event == EVENT_MBUTTONUP )
+    {
+      //  cout << "Middle button of the mouse is clicked - position (" << x << ", " << y << ")" << endl;
+    }
+
+}
+
+void OnSqrButtonClick(int state, void* userdata){
+    cout<<"Button state: " << state << endl;
 }
